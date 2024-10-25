17744c8cc82a194e4e3979d532f21d35341d5c4d
Simon <szymonpiechutowski@gmail.com>
2024-10-25 13:44:05 +0200

    initial

---
+++ reactivity_guide/refresh03.py
@@ -0,0 +1,30 @@
+from textual.app import App, ComposeResult
+from textual.reactive import reactive
+from textual.widget import Widget
+from textual.widgets import Input, Label
+
+
+class Name(Widget):
+    """Generates a greeting."""
+
+    who = reactive("name", recompose=True)
+
+    def compose(self) -> ComposeResult:
+        yield Label(f"Hello, {self.who}!")
+
+
+
+class WatchApp(App):
+    CSS_PATH = "refresh02.tcss"
+
+    def compose(self) -> ComposeResult:
+        yield Input(placeholder="Enter your name")
+        yield Name()
+
+    def on_input_changed(self, event: Input.Changed) -> None:
+        self.query_one(Name).who = event.value
+
+
+if __name__ == "__main__":
+    app = WatchApp()
+    app.run()
